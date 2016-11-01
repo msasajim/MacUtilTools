44d098183f5a933359480dc58a07256a2c545116
M Sasajima <msasajim@gmail.com>
2016-11-01 21:54:41 +0900

    update filename

--- forzip.sh
+++ forzip.sh
@@ -6,8 +6,9 @@ echo "check trash files..."
 find ./ -name "*.url" -print0 | xargs -0 rm
 find ./ -name "*.db" -print0 | xargs -0 rm
 find ./ -name "*.txt" -print0 | xargs -0 rm
+find ./ -name ".DS_Store" -print0 | xargs -0 rm
 
 echo "start for zip"
-for f in *; do echo "${f}"; cd "${f}"; zip -r ../"${f}.zip" * >/dev/null ; cd - >/dev/null; rm -r "${f}"; done
+for f in *; do echo "${f}"; cd "${f}"; rename -v 's/[^0-9]*//' *; zip -r ../"${f}.zip" * >/dev/null ; cd - >/dev/null; rm -r "${f}"; done
 
 echo "finished."
