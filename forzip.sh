da56b95b8e5e5ab389e0dfc3725abe4351e7fbfa
M Sasajima <msasajim@gmail.com>
2017-10-16 13:48:19 +0900

    update

--- forzip.sh
+++ forzip.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# initialize
 echo $PWD
 
+# check trash files
 echo "check trash files..."
 find ./ -name "*.url" -print0 | xargs -0 rm
 find ./ -name "*.db" -print0 | xargs -0 rm
@@ -9,7 +11,25 @@ find ./ -name "*.txt" -print0 | xargs -0 rm
 find ./ -name "*.rar" -print0 | xargs -0 rm
 find ./ -name ".DS_Store" -print0 | xargs -0 rm
 
+# process
 echo "start for zip"
-for f in *; do echo "${f}"; cd "${f}"; rename -v 's/[^0-9]*//' *; zip -r ../"${f}.zip" * >/dev/null ; cd - >/dev/null; rm -r "${f}"; done
+#for f in *; do echo "${f}"; cd "${f}"; rename -v 's/[^0-9]*//' *; zip -r ../"${f}.zip" * >/dev/null ; cd - >/dev/null; rm -r "${f}"; done
+for f in *;
+do
+  # move to the directory
+  echo "${f}"
+  cd "${f}"
+  # rename file
+  rename -v 's/[^0-9]*//' *
+  # zip file
+  zip -r ../"${f}.zip" * >/dev/null
+  # back to the directory
+  cd - >/dev/null
+  # delete the directory
+  rm -r "${f}"
+done
 
+
+# completed
 echo "finished."
+exit 0
