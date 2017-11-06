e068e00b8212f456e7be34afd9aea1ff7bb7b453
M Sasajima <msasajim@gmail.com>
2017-11-06 17:40:16 +0900

    update rename method 2

--- forzip.sh
+++ forzip.sh
@@ -10,8 +10,12 @@ find ./ -name "*.db" -print0 | xargs -0 rm
 find ./ -name "*.txt" -print0 | xargs -0 rm
 find ./ -name "*.rar" -print0 | xargs -0 rm
 find ./ -name ".DS_Store" -print0 | xargs -0 rm
-find ./ -name "単ページ" -print0 | xargs -0 rm -r
 find ./ -name "*.vix" -print0 | xargs -0 rm
+find ./ -name "*..*" -print0 | xargs -0 rm
+
+# check trach directories
+echo "check trash directories..."
+find ./ -name "単ページ" -print0 | xargs -0 rm -r
 
 # process
 echo "start for zip"
@@ -20,12 +24,11 @@ do
   # move to the directory
   echo "${f}"
   cd "${f}"
-  # delete folder
+  # delete folders
   find . -maxdepth 1 -mindepth 1 -type d -print0 | xargs -0 rm -r
   # rename file
   rename -v 's/[^0-9]*//' *
   # change filename to small capital
-  #for file in *; do cfile=`echo "${file}" | tr A-Z a-z`; mv "${file}" "${cfile}"; done
   rename -c -f *
   # zip file
   zip -r ../"${f}.zip" * >/dev/null
